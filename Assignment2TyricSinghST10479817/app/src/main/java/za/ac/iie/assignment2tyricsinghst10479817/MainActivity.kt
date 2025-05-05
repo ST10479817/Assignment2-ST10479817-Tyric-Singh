99105cda1bcc09d537011dc249719ca9066f2a4b
ST10479817 <st10479817@imconnect.edu.za>
2025-05-05 17:27:29 +0200

    Initial commit
    Set up project,
    start screen has been created.

---
+++ Assignment2TyricSinghST10479817/app/src/main/java/za/ac/iie/assignment2tyricsinghst10479817/MainActivity.kt
@@ -0,0 +1,38 @@
+package za.ac.iie.assignment2tyricsinghst10479817
+
+import android.os.Bundle
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        val mainLayout = findViewById<ViewGroup>(R.id.main)
+        val startButton = findViewById<Button>(R.id.btnstart)
+
+
+        startButton.setOnClickListener(
+            Toast.makeText(this)
+        )
+
+
+
+
+        mainLayout.setBackgroundResource(R.drawable.start_screen)
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
