f970f0e24cbd671410ee7bbea666e91e88c4eb49
ST10479817 <st10479817@imconnect.edu.za>
2025-05-06 19:08:26 +0200

    Fourth Commit
    I have add the score activity.
    Added the score output.
    Added the review and feedback output.
    Added two buttons exit and review.

--- Assignment2TyricSinghST10479817/app/src/main/java/za/ac/iie/assignment2tyricsinghst10479817/ScoreActivity.kt
+++ Assignment2TyricSinghST10479817/app/src/main/java/za/ac/iie/assignment2tyricsinghst10479817/ScoreActivity.kt
@@ -1,6 +1,8 @@
 package za.ac.iie.assignment2tyricsinghst10479817
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +13,62 @@ class ScoreActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_score)
+
+        val score = intent.getIntExtra("score", 0)
+        val userAnswers = intent.getBooleanArrayExtra("userAnswers")
+
+        val scoreText = findViewById<TextView>(R.id.txtscores)
+        val feedbackText = findViewById<TextView>(R.id.txtdisplayFeedback)
+        val reviewText = findViewById<TextView>(R.id.txtreview)
+        val reviewButton = findViewById<Button>(R.id.btnreview)
+        val exitButton = findViewById<Button>(R.id.btnexit)
+
+
+        scoreText.text = "$score/5"
+
+        feedbackText.text = if (score >= 3){
+            "Great job!"
+        } else {
+            "Keep practising"
+        }
+
+        reviewButton.setOnClickListener {
+            val questions = arrayOf(
+                "Nelson Mandela became South Africa's first Black president in 1994",
+                "The Soweto Uprising happened in 1980",
+                "Nelson Mandela was sentenced to life in prison",
+                "The ANC was banned again in 1994",
+                "Apartheid officially ended in 1994"
+            )
+
+            val answers = booleanArrayOf(
+                true,
+                false,
+                true,
+                false,
+                true
+            )
+
+            if (userAnswers == null) {
+                reviewText.text = "User answers not available"
+                return@setOnClickListener
+            }
+
+            val reviewContent = StringBuilder()
+            for (i in questions.indices){
+                val correct = userAnswers[i] == answers[i]
+                val result = if (correct) "Correct"  else "Incorrect"
+                reviewContent.append("Your answer was: ${userAnswers[i]} \nCorrect: ${answers[i]} \nResult: $result\n\n")
+            }
+            reviewText.text = reviewContent.toString()
+
+        }
+
+        exitButton.setOnClickListener {
+            finishAffinity()
+        }
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
