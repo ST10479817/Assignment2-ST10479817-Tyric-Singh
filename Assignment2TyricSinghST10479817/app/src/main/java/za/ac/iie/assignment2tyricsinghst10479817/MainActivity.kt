4575f94b8f4e85abbf89cae1af67463a40e414bf
ST10479817 <st10479817@imconnect.edu.za>
2025-05-19 20:26:41 +0200

    Ninth Commit
    Just adding extra information

--- Assignment2TyricSinghST10479817/app/src/main/java/za/ac/iie/assignment2tyricsinghst10479817/MainActivity.kt
+++ Assignment2TyricSinghST10479817/app/src/main/java/za/ac/iie/assignment2tyricsinghst10479817/MainActivity.kt
@@ -23,7 +23,8 @@ class MainActivity : AppCompatActivity() {
         val instructionsText = findViewById<TextView>(R.id.txtinstructions)
 
         // Text to guide the user on how to use the quiz
-        instructionsText.text =  "Press Start Quiz button to begin quiz. \nYou will have two options: True and False. \nThen press Next to continue to next question."
+        instructionsText.text =  "Press Start Quiz button to begin quiz. \nYou will have two options: True or False. Please pick one." +
+                "\nThen press Next to continue to next question."
 
         startButton.setBackgroundColor(Color.MAGENTA)//Colour of the button
 
